55f9f1aadfc20c9c525f62a67711c8d343629356
Wei Qian <68811186+Ebion@users.noreply.github.com>
2023-09-25 02:21:24 +0800

    Time: 1 ms (89.84%), Space: 40.8 MB (27.05%) - LeetHub

---
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int c=0;
+        String add=word;
+        while(sequence.contains(word)==true)
+        {
+            c+=1;
+            word=word+add;
+        }
+        return c;
+    }
+}
