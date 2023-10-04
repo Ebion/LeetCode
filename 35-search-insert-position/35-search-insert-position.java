67785708bc90676c43b21d83739af66553f88259
Wei Qian <68811186+Ebion@users.noreply.github.com>
2023-10-05 01:16:19 +0800

    Time: 0 ms (100.00%), Space: 43.3 MB (41.43%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -1,15 +1,22 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        int length = nums.length;
+        
         int start = 0;
-        int end = nums.length-1;
-
-        while (start <= end) {
-            int mid = start + (end-start)/2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) end = mid-1;
-            else start = mid+1;
+        int end = length - 1;
+        
+        while(end >= start) {
+            int midpoint = start + (end - start)/2;
+            // System.out.println(nums[midpoint]);
+            if (nums[midpoint] == target) {
+                return midpoint;
+            } else if (nums[midpoint] > target) {
+                end = midpoint - 1;
+            } else {
+                start = midpoint + 1;
+            } 
         }
-
+        
         return start;
     }
 }
